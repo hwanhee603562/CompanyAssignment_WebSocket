212ec2c83ba55c7f75b58892ec0c99d22a90a111
이환희 <hwanhee603562@gmail.com>
2024-01-03 11:36:07 +0900

    2024-01-03 11:35 검토완료 보완사항은 아래와 같음

--- WebChatting/src/main/resources/static/js/chattingRoom.js
+++ WebChatting/src/main/resources/static/js/chattingRoom.js
@@ -1,39 +1,44 @@
 const socket = new WebSocket("ws://192.168.0.80:8085/chattingRoom");
+const now = new Date().toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', hour12: true});
 
 $(document).ready(function() {
 	// 소켓생성
-	socket.onopen = function(event) {
-		console.log("WebSocket 연결 성공", event);
-	};
-
-	// 메시지 받음
-	socket.onmessage = function(event) {
-		const newLeftBox = $("<div class='leftBox'>");
-		const lTime = $("<span class='lTime'></span>").text(new Date());
-		const lName = $("<div class='lName'>나의이름</div>").text(new URLSearchParams(window.location.search).get("userId"));
-		const lContent = $("<span class='lContent'></span>").text(event.data);
-
-		newLeftBox.append(lName);
-		newLeftBox.append("<div>").append(lContent).append(lTime);
-		$(".chatWrap").append(newLeftBox);
-	};
-
-	// 소켓소멸
-	socket.onclose = function(event) {
-		console.log("WebSocket 연결이 닫혔습니다.", event);
-	};
-
-	// 에러발생
-	socket.onerror = function(event) {
-		console.error("WebSocket 오류 발생", event);
+	socket.onopen = function(e) {
+		console.log("WebSocket 연결 성공", e);
 	};
 });
 
+// 메시지 수령
+socket.onmessage = function(e) {
+	const newLeftBox = $("<div class='leftBox'>");
+	const lTime = $("<span class='lTime'></span>").text(now);
+	const lName = $("<div class='lName'>나의이름</div>").text(new URLSearchParams(window.location.search).get("userId"));
+	const lContent = $("<span class='lContent'></span>").text(e.data);
+
+	newLeftBox.append(lName);
+	newLeftBox.append("<div>").append(lContent).append(lTime);
+	$(".chatWrap").append(newLeftBox);
+};
+// 소켓소멸
+socket.onclose = function(e) {
+	console.log("WebSocket 연결이 닫혔습니다.", e);
+};
+
+// 에러발생
+socket.onerror = function(e) {
+	console.error("WebSocket 오류 발생", e);
+};
+
+$(".inputBox").keypress(function(e) {
+	if (e.which === 13) sendMessage();
+});
+
+// 메시지 전송
 function sendMessage() {
 	const message = $(".inputBox").val();
 	const newRightBox = $("<div class='rightBox'>");
 	const rName = $("<div class='rName'>나의이름</div>").text(new URLSearchParams(window.location.search).get("userId"));
-	const rTime = $("<span class='rTime'></span>").text(new Date());
+	const rTime = $("<span class='rTime'></span>").text(now);
 	const rContent = $("<span class='rContent'></span>").text(message);
 
 	newRightBox.append(rName);
@@ -41,4 +46,5 @@ function sendMessage() {
 	$(".chatWrap").append(newRightBox);
 
 	socket.send(message);
+	$(".inputBox").val("");
 }
--- WebChatting/src/main/resources/static/js/join.js
+++ WebChatting/src/main/resources/static/js/join.js
@@ -1,4 +1,4 @@
 $(".joinBtn").click(function() {
 	var userId = $(".inputId").val();
-	window.location.href = "http://localhost:8085/chatting?userId="+userId;
+	window.location.href = "http://192.168.0.80:8085/chatting?userId="+userId;
 });
